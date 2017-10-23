0f9caef49c3112e66acc29ef71e5bc429409ba22
github username <github email address>
2017-10-22 21:48:39 -0700

    add pages and modify app.rb

--- app.rb
+++ app.rb
@@ -2,6 +2,12 @@ require 'sinatra'
 
 #test integrated source control
 
+def page_content(title)
+    File.read("pages/#{title}.txt"
+rescue Error::ENDENT
+    return nil
+end
+
 get "/" do
     erb :welcome
 end
